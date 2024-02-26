4077471624ca52d4693bec165e79c142e1e0ad24
Quraishi <furqan.quraishi@bt.com>
2024-02-26 16:13:36 +0000

    My new commit

---
+++ backups1.sh
@@ -0,0 +1,28 @@
+My name is Fq and I am learning new tech for future. 
+
+This is my first step to learn Git and GitHub.
+
+DevOps roadmap for 3 months.
+
+1. Learn about Devops, why devops, technology and tools used.
+
+2. Git and Git Hub
+
+3. Linux commands used in devops
+
+4.Shell scripting
+
+5.Jenkins for CI/CD
+
+6. Docker for containterization
+
+7. Kubernetes for deployment
+
+8. Ansible
+
+9. Terraform for IAC.
+
+10. Any cloud service provide- GCP/AWS/MS Azure.
+
+11. Interview prepartion
+
